a0a9bcfb036b0dfd8c961a60fba3de3bc9075e66
arnob20 <40712303+arnob20@users.noreply.github.com>
2022-11-30 14:19:00 +0600

    Update PdfViewerActivity.kt

--- pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewerActivity.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewerActivity.kt
@@ -88,6 +88,10 @@ class PdfViewerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window
+            .setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_pdf_viewer)
 
         setUpToolbar(
